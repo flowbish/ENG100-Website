84327133f157ad4b97470e5af428ea726c17e69e
Porter Smith <flowbish@gmail.com>
2013-09-10 19:56:15 -0500

    changed style, added animations

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,37 +1,46 @@
 $(document).ready(function(){
+	var running = false;
 	$("#projects .info-side-button").click(function(){
-		$("#about .info-main-panel").animate({
-			opacity:0
-		}, 0);
-		$("#contact .info-main-panel").animate({
-			opacity:0
-		}, 0);
-		$("#projects .info-main-panel").animate({
-			opacity:100
-		}, 0);
+		if (!running) {
+			running = true;
+			$("#about .info-main-panel").animate({top:-430}, 1000, function(){
+				$("#about").css("z-index","5")
+			}).animate({top:-30}, 1000);
+			
+			$("#contact .info-main-panel").animate({top:-430}, 1000, function(){
+				$("#contact").css("z-index","0")
+			}).animate({top:-30}, 1000);
+			
+			$("#projects").delay(1000).css("z-index","10").delay(1000,
+														function(){running = false;})
+		}
     });
 	
 	$("#contact .info-side-button").click(function(){
-		$("#about .info-main-panel").animate({
-			opacity:0
-		}, 0);
-		$("#contact .info-main-panel").animate({
-			opacity:100
-		}, 0);
-		$("#projects .info-main-panel").animate({
-			opacity:0
-		}, 0);
+		if (!running) {
+			$("#about .info-main-panel").animate({top:-430}, 1000, function(){
+				$("#about").css("z-index","5")
+			}).animate({top:-30}, 1000);
+			
+			$("#projects .info-main-panel").animate({top:-430}, 1000, function(){
+				$("#projects").css("z-index","0")
+			}).animate({top:-30}, 1000);
+			
+			$("#contact").delay(1000).css("z-index","10")
+		}
     });
 	
 	$("#about .info-side-button").click(function(){
-		$("#contact .info-main-panel").animate({
-			opacity:0
-		}, 0);
-		$("#projects .info-main-panel").animate({
-			opacity:0
-		}, 0);
-		$("#about .info-main-panel").animate({
-			opacity:100
-		}, 0);
+		if (!running) {
+			$("#projects .info-main-panel").animate({top:-430}, 1000, function(){
+				$("#projects").css("z-index","5")
+			}).animate({top:-30}, 1000);
+			
+			$("#contact .info-main-panel").animate({top:-430}, 1000, function(){
+				$("#contact").css("z-index","0")
+			}).animate({top:-30}, 1000);
+			
+			$("#about").delay(1000).css("z-index","10")
+		}
     });
 });
