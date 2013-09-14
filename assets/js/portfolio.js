87e2e54e537c791352d2aa8cd5111716f6294761
Porter Smith <flowbish@gmail.com>
2013-09-13 22:17:20 -0500

    removed animations, added google fonts

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,46 +1,17 @@
 $(document).ready(function(){
-	var running = false;
-	$("#projects .info-side-button").click(function(){
-		if (!running) {
-			running = true;
-			$("#about .info-main-panel").animate({top:-430}, 1000, function(){
-				$("#about").css("z-index","5")
-			}).animate({top:-30}, 1000);
-			
-			$("#contact .info-main-panel").animate({top:-430}, 1000, function(){
-				$("#contact").css("z-index","0")
-			}).animate({top:-30}, 1000);
-			
-			$("#projects").delay(1000).css("z-index","10").delay(1000,
-														function(){running = false;})
-		}
+	$("#about .info-side-button").click(function(){	
+		$("#about").css("z-index","10");
+		$("#projects").css("z-index","5");
+		$("#contact").css("z-index","0");
     });
-	
-	$("#contact .info-side-button").click(function(){
-		if (!running) {
-			$("#about .info-main-panel").animate({top:-430}, 1000, function(){
-				$("#about").css("z-index","5")
-			}).animate({top:-30}, 1000);
-			
-			$("#projects .info-main-panel").animate({top:-430}, 1000, function(){
-				$("#projects").css("z-index","0")
-			}).animate({top:-30}, 1000);
-			
-			$("#contact").delay(1000).css("z-index","10")
-		}
+	$("#projects .info-side-button").click(function(){	
+		$("#projects").css("z-index","10");
+		$("#about").css("z-index","5");
+		$("#contact").css("z-index","0");
     });
-	
-	$("#about .info-side-button").click(function(){
-		if (!running) {
-			$("#projects .info-main-panel").animate({top:-430}, 1000, function(){
-				$("#projects").css("z-index","5")
-			}).animate({top:-30}, 1000);
-			
-			$("#contact .info-main-panel").animate({top:-430}, 1000, function(){
-				$("#contact").css("z-index","0")
-			}).animate({top:-30}, 1000);
-			
-			$("#about").delay(1000).css("z-index","10")
-		}
+	$("#contact .info-side-button").click(function(){	
+		$("#contact").css("z-index","10");
+		$("#about").css("z-index","5");
+		$("#projects").css("z-index","0");
     });
 });
